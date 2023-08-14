61b39923db3e72e2bb26a1c1bfb7f4959bf5c9e0
anton_zeltser <anton.zeltser@interactive-tools.de>
2023-08-14 21:00:48 +0200

    added Status for the Requests

--- src/main/java/tron/one/controller/NavigationEndpoint.java
+++ src/main/java/tron/one/controller/NavigationEndpoint.java
@@ -2,12 +2,20 @@ package tron.one.controller;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.gson.Gson;
+
+import javax.ws.rs.core.Response;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tron.one.dto.NavigationDTO;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @RestController("/")
 public class NavigationEndpoint {
@@ -16,15 +24,58 @@ public class NavigationEndpoint {
 
         return "test person222+++++";
     }
-    @GetMapping("/navi")
-    public String getNavigation() {
-        NavigationDTO navi = new NavigationDTO().builder()
-                .title("title")
-                .uri("www.google.com")
-                .teaser("teaser")
-                .active(true)
-                .children(Collections.emptyList()).build();
-        Gson gson = new Gson();
-        return gson.toJson(navi);
+
+    @GetMapping("/navi/")
+    public ResponseEntity getNavigation() {
+        try {
+            List<NavigationDTO> childrens = new ArrayList<>();
+            childrens.add(
+                    new NavigationDTO().builder()
+                            .title("children")
+                            .uri("www.google.com")
+                            .teaser("teaser")
+                            .active(true)
+                            .children(Collections.emptyList()).build()
+            );
+
+            childrens.add(
+                    new NavigationDTO().builder()
+                            .title("children")
+                            .uri("www.google.com")
+                            .teaser("teaser")
+                            .active(true)
+                            .children(Collections.emptyList()).build()
+            );
+
+            NavigationDTO navi = new NavigationDTO().builder()
+                    .title("title")
+                    .uri("www.google.com")
+                    .teaser("teaser")
+                    .active(true)
+                    .children(
+                            //Collections.emptyList()
+                            childrens
+                    ).build();
+
+
+            Gson gson = new Gson();
+            return ResponseEntity
+                    .status(HttpStatus.ACCEPTED)
+                    .body(gson.toJson(navi));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Bad Request: Invalid endpoint");
+        }
+
+
+    }
+
+    @RequestMapping("/**") // Catch-all mapping for undefined endpoints
+    public ResponseEntity<String> handleBadRequests() {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body("Bad Request: Invalid endpoint");
     }
 }
