4c6c3b255709ab611ba23a79ea9ef6e3d492f06d
anton_zeltser <anton.zeltser@interactive-tools.de>
2023-08-17 12:52:48 +0200

    to do: test block

--- src/test/java/tron/one/OneApplicationTests.java
+++ src/test/java/tron/one/OneApplicationTests.java
@@ -1,13 +1,6 @@
 package tron.one;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class OneApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
